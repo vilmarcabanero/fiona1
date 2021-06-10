db6511cc5a7fdea5e114b7efaaa16e49e9bf435c
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-06-10 20:08:33 +0800

    Installed styled-components and material-ui

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: `https://entropiya-server.herokuapp.com/`
-  baseURL: `http://localhost:5000/`
+  baseURL: `http://localhost:4000/`
 });
